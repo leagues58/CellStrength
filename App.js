6cdf58fa6d2d416fefe894033f900230ab3f37a5
Eric <leagues58@gmail.com>
2020-05-25 14:07:32 -0400

    Add cellstrength js module and call in app. App displays cell strength!

--- App.js
+++ App.js
@@ -3,16 +3,33 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+import CellStrength from './CellStrength';
+import {request, PERMISSIONS} from 'react-native-permissions';
 
 const App = () => {
+  const [cellStrength, setCellStrength] = useState(0);
+
+  request(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION).then(result => {
+    if (result === 'granted') {
+      setInterval(() => {
+        CellStrength.getCellStrength().then(data => {
+          if (data) {
+            setCellStrength(data);
+          }
+        });
+      }, 200);
+    }
+  });
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.content}>
-        <View>
-          <Text>Hello!</Text>
+        <View style={styles.readoutContainer}>
+          <Text style={styles.readout}>{cellStrength}</Text>
+          <Text style={styles.unit}>dBm</Text>
         </View>
       </SafeAreaView>
     </>
@@ -24,6 +41,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  readoutContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  readout: {
+    fontSize: 85,
+  },
+  unit: {
+    fontSize: 35,
+    marginBottom: 15,
   },
 });
 
